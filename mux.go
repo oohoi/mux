959b13709944968d87b6f5fd79b42b997cb5fd49
xer <2mr.long@gmail.com>
2019-04-28 12:42:44 +0800

    Update mux.go
    line 368: var SkipRouter --> var ErrSkipRouter
    line 378: if err == SkipRouter { -->	if err == ErrSkipRouter {
    
    sorry, i dont know how to delete my last edit 30 minutes before, this is the same new one

--- mux.go
+++ mux.go
@@ -365,7 +365,10 @@ func (r *Router) Walk(walkFn WalkFunc) error {
 
 // SkipRouter is used as a return value from WalkFuncs to indicate that the
 // router that walk is about to descend down to should be skipped.
-var SkipRouter = errors.New("skip this router")
+// -- var SkipRouter = errors.New("skip this router") -- original
+
+// ErrSkipRouter is used as a return value from WalkFuncs to indicate that the
+var ErrSkipRouter = errors.New("skip this router") // -- begin with Err, according to golint
 
 // WalkFunc is the type of the function called for each route visited by Walk.
 // At every invocation, it is given the current route, and the current router,
@@ -375,7 +378,8 @@ type WalkFunc func(route *Route, router *Router, ancestors []*Route) error
 func (r *Router) walk(walkFn WalkFunc, ancestors []*Route) error {
 	for _, t := range r.routes {
 		err := walkFn(t, r, ancestors)
-		if err == SkipRouter {
+		// if err == SkipRouter { // -- original
+		if err == ErrSkipRouter { // -- modify
 			continue
 		}
 		if err != nil {
